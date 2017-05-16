bdb70a7562a67896b5cb4a1a1f4334ade1fb82af
Hiep Nguyen <ngochiep9523@gmail.com>
2017-05-16 13:45:48 +0700

    Change routes

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
 var trip = require('./routes/trip');
 
 var app = express();
@@ -22,8 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/trip', trip);
+app.use('/', trip);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
